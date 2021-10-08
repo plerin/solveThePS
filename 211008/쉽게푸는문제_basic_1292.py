c8c4c4d4c5ce2f5f4e678e51200908278d9d4055
con <zborisz@naver.com>
2021-10-08 22:59:58 +0900

    Update today's solved problem

---
+++ 211008/쉽게푸는문제_basic_1292.py
@@ -0,0 +1,14 @@
+'''
+goal : 특정 수열에서 A,B번째 숫자까의 합 구하기
+1. 입력 받기
+    1) A,B : 둘다 INT
+로직
+    1) 규칙에 맞게 수열 리스트에 담는다.
+    2) slicing으로 자른다.
+'''
+
+#!
+A, B = list(map(int, input().split()))
+
+# 2
+lst = for i in range(1, 1000)
